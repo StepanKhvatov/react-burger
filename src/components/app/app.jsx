7332e10afe35de2fd775dd49de89e87944229b7e
Stepan <stepanhv@yandex.ru>
2022-08-11 21:53:54 +0300

    Create env file. Added api data to App component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import appStyles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import ConstructorPage from "../constuctor-page/constructor-page";
-import { ingredients } from "../../utils/data";
 
 const App = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/api/ingredients`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res.success) {
+          return setIngredients(res.data);
+        }
+
+        return res;
+      })
+      .catch((error) => {
+        console.log("Fetch data error", error.message);
+      });
+  }, []);
+
   return (
     <div className={appStyles.app}>
       <AppHeader />
-      <ConstructorPage ingredients={ingredients} />
+      {!!ingredients.length && <ConstructorPage ingredients={ingredients} />}
     </div>
   );
 };
