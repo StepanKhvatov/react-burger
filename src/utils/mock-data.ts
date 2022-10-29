9f469dfb3b9f26cd1ba0aadb10182b5ee1b6b120
Stepan <stepanhv@yandex.ru>
2022-10-30 04:38:38 +0700

    Added ingredients constructor reducer tests

--- src/utils/mock-data.ts
+++ src/utils/mock-data.ts
@@ -111,6 +111,21 @@ export const mockIngredient: TIngredient = {
   __v: 0,
 };
 
+export const mockSauceIngredient: TIngredient = {
+  _id: "60d3b41abdacab0026a733cd",
+  name: "Соус фирменный Space Sauce",
+  type: "sauce",
+  proteins: 50,
+  fat: 22,
+  carbohydrates: 11,
+  calories: 14,
+  price: 80,
+  image: "https://code.s3.yandex.net/react/code/sauce-04.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/sauce-04-mobile.png",
+  image_large: "https://code.s3.yandex.net/react/code/sauce-04-large.png",
+  __v: 0,
+};
+
 export const mockIngredients: Array<TIngredient> = [
   {
     _id: "60d3b41abdacab0026a733c6",
